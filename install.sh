07c2bfa7255fac2ea53b755088e1badc4bd3c23b
Jesse Lovitt <jlovitt@localhost.localdomain>
2016-11-27 20:31:55 -0700

    added chown to install

--- install.sh
+++ install.sh
@@ -1,15 +1,21 @@
 #/bin/sh
 
 if [ $# -ne 2 ]; then
-	echo "USE: install.sh <path to install static files> <path to install django>"
+	echo "USE: install.sh <path to install static files> <path to install django> <uid:gid to chown to> "
 	exit
 fi
 
 echo "cp -r static/* $1/"
 cp -r static/* $1
+echo "chown -R $3 $1"
+chown -R $3 $1
 
 echo "cp -r django/* $2/"
 cp -r django/* $2
+echo "chown -R $3 $2"
+chown -R $3 $2
 
-echo "Remember to chown the copied files to match the permissions needed by your web server."
+
 echo "Remember to add ALLOWED_HOSTS to the django settings"
+echo "Remember to change path to images"
+
